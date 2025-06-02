cb51b992eceab783f396c3a91513e7f8208658b3
nhmikami <nhmikami@gmail.com>
2025-06-02 18:28:29 -0300

    rank 04

--- exam-rank-04/microshell.c
+++ exam-rank-04/microshell.c
@@ -1,121 +1,80 @@
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <unistd.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <string.h> 
 
-int	ft_error(char *str)
+int	err(char *str)
 {
 	while (*str)
 		write(2, str++, 1);
-	return (1);
+	return 1;
 }
 
-int exec_cd(char **av, int i)
+int	exec_cd(char **av, int i)
 {
 	if (i != 2)
-		return (ft_error("error: cd: bad arguments\n"));
-	if (chdir(av[1]) == -1)
+		return err("error: cd: bad arguments\n");
+	else if (chdir(av[1]) == -1)
+		return err("error: cd: cannot change directory to "), err(av[1]), err("\n");
+	return 0;
+}
+
+int	exec_cmd(char **av, char **ev, int i)
+{
+	int	fd[2];
+	int	status;
+	int	has_pipe = av[i] && !strcmp(av[i], "|");
+
+	if (has_pipe && pipe(fd) == -1)
+		return err("error: fatal\n");
+
+	int pid = fork();
+	if (!pid)
 	{
-		ft_error("error: cd: cannot change directory to ");
-		ft_error(av[1]);
-		ft_error("\n");
-		return (1);
+		av[i] = 0;
+		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return err("error: fatal\n");
+		execve(*av, av, ev);
+		err("error: cannot execute "), err(*av), err("\n"), exit(1);
 	}
-	return (0);
-}
-
-int	exec_cmd(char **av, int i, char **ev, int *fd_in)
-{
-	int	pid;
-	int	pipe_fd[2];
-	int	status = 0;
-	int	has_pipe = 0;
-
-	// verifica se há pipe
-	if (av[i] && !strcmp(av[i], "|"))
-		has_pipe = 1;
-	if (has_pipe && pipe(pipe_fd) == -1)
-		return (ft_error("error: fatal\n"));
-
-	// cria o processo filho
-	pid = fork();
-	if (pid == -1)
-		return (ft_error("error: fatal\n"));
-
-	// processo filho
-	if (pid == 0) {
-		av[i] = NULL;
-		// redireciona a entrada padrão para fd_in
-		if (dup2(*fd_in, 0) == -1) {
-			ft_error("error: fatal\n");
-			exit (1);
-		}
-		close(*fd_in);
-
-		if (has_pipe) {
-			// redireciona a saída padrão para o lado de escrita do pipe
-			if (dup2(pipe_fd[1], 1) == -1) {
-				close(pipe_fd[0]);
-				close(pipe_fd[1]);
-				ft_error("error: fatal\n");
-				exit (1);
-			}
-			close(pipe_fd[0]);
-			close(pipe_fd[1]);
-		}
-		// executa o comando
-		execve(av[0], av, ev);
-		ft_error("error: cannot execute ");
-		ft_error(av[0]);
-		ft_error("\n");
-		exit(127);
-	}
-
-	// processo pai
-	if (has_pipe) {
-		// redireciona o fd_in para o lado de leitura do pipe
-		if (dup2(pipe_fd[0], *fd_in) == -1)
-			return (ft_error("error: fatal\n"));
-		close(pipe_fd[1]);
-		close(pipe_fd[0]);
-	}
-	else {
-		// redireciona o fd_in para a entrada padrão
-		if (dup2(0, *fd_in) == -1)
-			return (ft_error("error: fatal\n"));
-	}
-
 	if (waitpid(pid, &status, 0) == -1)
-		return (ft_error("error: fatal\n"));
-    if (WIFEXITED(status))
+		return err("error: fatal\n");
+	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		return err("error: fatal\n");
+	if (WIFEXITED(status))
 		return WEXITSTATUS(status);
-	return (1);
+	return 1;
 }
 
-int	main(int ac, char **av, char **ev)
+int	main(int ac, char **av, char **ep)
 {
-	(void)ac;
 	int	i = 1;
 	int	status = 0;
-	int	fd_in = dup(0);
-	
-	if (fd_in < 0)
-		return (ft_error("error: fatal\n"));
-	while (av[i])
-	{
-		av = av + i;
-		i = 0;
-		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
-			i++;
-		if (!strcmp(av[0], "cd"))
-			status = exec_cd(av, i);
-		else
-			status = exec_cmd(av, i, ev, &fd_in);
-		if (av[i])
-			i++;
-	}
-	close(fd_in);
-	return (status);
-}
+	int	fd_stdin = dup(0);
+	if (fd_stdin == -1)
+		return err("error: fatal\n");
 
-// valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all --track-fds=yes ./a.out  /bin/ls | /bin/grep micro
+	if (ac > 1)
+	{
+		while (av[i])
+		{
+			av += i;
+			i = 0;
+			while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+				i++;
+			if (!strcmp(av[0], "cd"))
+				status = exec_cd(av, i);
+			else
+				status = exec_cmd(av, ep, i);
+			if (!av[i] || !strcmp(av[i], ";"))
+			{
+				if (dup2(fd_stdin, 0) == -1)
+					return err("error: fatal\n");
+			}
+			if (av[i])
+				i++;
+		}
+	}
+	close(fd_stdin);
+	return status;
+}
