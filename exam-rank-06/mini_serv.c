7553c793a5caaee28e12d4d271f8618b4380751b
nhmikami <nhmikami@gmail.com>
2026-07-21 17:43:44 -0300

    rank 06

--- exam-rank-06/mini_serv.c
+++ exam-rank-06/mini_serv.c
@@ -7,25 +7,28 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+int		sockfd;
 int		count = 0;
 int		max_fd = 0;
 int		ids[1024];
-char	*msgs[1024];
-char	buf_read[1024];
-char	buf_write[42];
-fd_set	rfds, wfds, afds; 
-// conjunto de fds:
-// - rfds: fds prontos para leitura (socket de escuta + clientes conectados)
-// - wfds: fds prontos para escrita (clientes conectados)
-// - afds: fds ativos (clientes conectados + socket de escuta)
+char	*inbox[1024];	// incoming messages
+char	*outbox[1024];	// outgoing messages
+char	read_buf[1024];
+char	write_buf[42];
+fd_set	afds, rfds, wfds;
+// fds set:
+// - afds: active fds (server socket + connected clients)
+// - rfds: fds ready to read
+// - wfds: fds ready to write
 
-int	extract_message(char **buf, char **msg) {
-	char	*newbuf;
-	int		i;
+
+int extract_message(char **buf, char **msg) {
+	char *newbuf;
+	int i;
 
 	*msg = 0;
 	if (*buf == 0)
-		return 0;
+		return (0);
 
 	i = 0;
 	while ((*buf)[i]) {
@@ -44,15 +47,15 @@ int	extract_message(char **buf, char **msg) {
 	return (0);
 }
 
-char	*str_join(char *buf, char *add) {
-	char	*newbuf;
-	int		len;
+char *str_join(char *buf, char *add) {
+	char *newbuf;
+	int len;
 
 	if (buf == 0)
 		len = 0;
 	else
 		len = strlen(buf);
-		
+
 	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
 	if (newbuf == 0)
 		return (0);
@@ -64,49 +67,75 @@ char	*str_join(char *buf, char *add) {
 	return (newbuf);
 }
 
-void	fatal_error() {
+void fatal_error(void) {
 	write(2, "Fatal error\n", 12);
 	exit(1);
 }
 
-void	notify_others(int fd, char *msg) {
+void notify_others(int fd, char *msg) {
 	for (int i = 0; i <= max_fd; i++) {
-		if (FD_ISSET(i, &wfds) && i != fd) // verifica se fd está ativo no conjunto
-			send(i, msg, strlen	(msg), 0);
+		if (FD_ISSET(i, &afds) && i != fd && i != sockfd) {
+			outbox[i] = str_join(outbox[i], msg);
+			if (!outbox[i])
+				fatal_error();
+		}
 	}
 }
 
-void	send_msg(int fd) {
-	char	*msg;
+void send_msg(int fd) {
+	char *msg;
+	int ret;
 
-	while (extract_message(&msgs[fd], &msg) > 0) {
-		sprintf(buf_write, "client %d: ", ids[fd]);
-		notify_others(fd, buf_write);
+	while (ret = extract_message(&inbox[fd], &msg) > 0) {
+		sprintf(write_buf, "client %d: ", ids[fd]);
+		notify_others(fd, write_buf);
 		notify_others(fd, msg);
 		free(msg);
 	}
+	if (ret < 0)
+		fatal_error();
 }
 
-void	accept_client(int fd) {
+void flush_client(int fd) {
+	if (!outbox[fd] || !outbox[fd][0])
+		return ;
+
+	char *rest;
+	int len = strlen(outbox[fd]);
+	int sent = send(fd, outbox[fd], len, 0);
+	if (sent > 0) {
+		rest = malloc(len - sent + 1);
+		if (!rest)
+			fatal_error();
+		strcpy(rest, outbox[fd] + sent);
+		free(outbox[fd]);
+		outbox[fd] = rest;
+	}
+}
+
+void accept_client(int fd) {
 	if (fd > max_fd)
 		max_fd = fd;
 	ids[fd] = count++;
-	msgs[fd] = 0;
-	FD_SET(fd, &afds); // adiciona o fd ao conjunto
-	sprintf(buf_write, "server: client %d just arrived\n", ids[fd]);
-	notify_others(fd, buf_write);
+	inbox[fd] = 0;
+	outbox[fd] = 0;
+	FD_SET(fd, &afds);
+	sprintf(write_buf, "server: client %d just arrived\n", ids[fd]);
+	notify_others(fd, write_buf);
 }
 
-void	disconnect_client(int fd) {
-	sprintf(buf_write, "server: client %d just left\n", ids[fd]);
-	notify_others(fd, buf_write);
-	free(msgs[fd]);
-	msgs[fd] = 0;
-	FD_CLR(fd, &afds); // remove o fd do conjunto
+void disconnect_client(int fd) {
+	sprintf(write_buf, "server: client %d just left\n", ids[fd]);
+	FD_CLR(fd, &afds);
+	notify_others(fd, write_buf);
+	free(inbox[fd]);
+	inbox[fd] = 0;
+	free(outbox[fd]);
+	outbox[fd] = 0;
 	close(fd);
 }
 
-int create_socket() {
+int create_socket(void) {
 	max_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (max_fd < 0)
 		fatal_error();
@@ -114,55 +143,65 @@ int create_socket() {
 	return (max_fd);
 }
 
-int	main(int ac, char **av) {
+int main(int ac, char **av) {
 
 	if (ac != 2) {
 		write(2, "Wrong number of arguments\n", 26);
 		exit(1);
 	}
 
-	FD_ZERO(&afds); // limpa o conjunto de fds
-	int	sockfd = create_socket(); // socket de escuta do servidor
+	FD_ZERO(&afds);
+	sockfd = create_socket();	// server socket
 
 	struct sockaddr_in servaddr;
 	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(2130706433);
-	servaddr.sin_port = htons(atoi(av[1]));	// replace 8081 with atoi(av[1])
+	servaddr.sin_family = AF_INET;					// IPv4
+	servaddr.sin_addr.s_addr = htonl(2130706433);	// 127.0.0.1
+	servaddr.sin_port = htons(atoi(av[1]));			// port - received from command line argument
 
-	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)))
+	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) != 0)
 		fatal_error();
-	if (listen(sockfd, 10))
+	if (listen(sockfd, 10) != 0)
 		fatal_error();
 
 	while (42) {
-		rfds = wfds = afds;
+		rfds = afds;
+		FD_ZERO(&wfds);
+
+		for (int fd = 0; fd <= max_fd; fd++) {
+			if (outbox[fd] && outbox[fd][0])
+				FD_SET(fd, &wfds);
+		}
+
 		if (select(max_fd + 1, &rfds, &wfds, NULL, NULL) < 0)
 			fatal_error();
 
 		for (int fd = 0; fd <= max_fd; fd++) {
-			if (!FD_ISSET(fd, &rfds))
+			if (fd == sockfd && FD_ISSET(fd, &rfds)) {	// server
+				struct sockaddr_in cliaddr;
+				socklen_t len = sizeof(cliaddr);
+				int connfd = accept(sockfd, (struct sockaddr *)&cliaddr, &len);	// client socket
+				if (connfd >= 0)
+					accept_client(connfd);
 				continue;
+			}
 
-			if (fd == sockfd) { // servidor
-				socklen_t len = sizeof(servaddr);
-				int client_fd = accept(sockfd, (struct sockaddr *)&servaddr, &len); // socket para o cliente 
-				if (client_fd >= 0) {
-					accept_client(client_fd);
-					break;
-				}
-			} 
-			else { // cliente
-				int read_bytes = recv(fd, buf_read, 1000, 0);
-				if (read_bytes <= 0) {
+			if (FD_ISSET(fd, &wfds))	// ready to write
+				flush_client(fd);
+
+			if (FD_ISSET(fd, &rfds)) {	// ready to read
+				int bytes_read = recv(fd, read_buf, 1000, 0);
+				if (bytes_read <= 0) {
 					disconnect_client(fd);
-					break;
+					continue;
 				}
-				buf_read[read_bytes] = 0;
-				msgs[fd] = str_join(msgs[fd], buf_read);
+				read_buf[bytes_read] = 0;
+				inbox[fd] = str_join(inbox[fd], read_buf);
+				if (!inbox[fd])
+					fatal_error();
 				send_msg(fd);
 			}
 		}
 	}
-	return 0;
+	return (0);
 }
